b81dc1583d2f74d00968489a018e24e2f7bd6f5d
sabs21 <nms00010101@outlook.com>
2021-07-19 16:59:03 -0400

    added spiral to mess around with

--- 4/shader.js
+++ 4/shader.js
@@ -8,6 +8,9 @@ import * as THREE from "./three.module.js";
 import { GLTFLoader } from './GLTFLoader.js';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const deg2rad = function (deg) {
+        return deg * Math.PI / 180;
+    }
     // Find the dimensions of the viewport for three JS
     const threeDisplay = document.getElementById("threeDisplay");
     const threeDisplayRect = threeDisplay.getBoundingClientRect();
@@ -23,7 +26,12 @@ document.addEventListener("DOMContentLoaded", () => {
         0.1, // near plane
         1000 // far plane
     );
-    camera.position.z = 4;
+    camera.position.x = -4;
+    camera.position.y = 18;
+    camera.position.z = 0;
+    camera.rotation.x = deg2rad(270); //-1.2;
+    camera.rotation.y = deg2rad(300); //-0.6;
+    camera.rotation.z = deg2rad(340); //0.3;
 
     // Setup a point light
     const light = new THREE.DirectionalLight( 0xffffff, 1.0 );
@@ -40,15 +48,18 @@ document.addEventListener("DOMContentLoaded", () => {
     var currentTime = 0;
 
     // Setup animate function
-    var monkey = null;
+    var helix = null;
+    var cameraRot = 0;
     const animate = function (timestamp) {
         requestAnimationFrame(animate);
         //console.log("Timestamp: ", timestamp);
         currentTime = timestamp/1000; // Current time in seconds.
 
         // Update rotation
-        monkey.rotation.x += 0.01;
-        monkey.rotation.y += 0.01;
+        //helix.rotation.x += 0.006;
+        helix.rotation.z += 0.006;
+        //cameraRot += 0.01;
+        //camera.rotation.x = Math.sin(cameraRot);
         
         //console.log(monkey);
         //monkey.material.uniforms.time.value = currentTime;
@@ -61,7 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // If all the models are loaded, then animate.
     const attemptToAnimate = function() {
-        if (monkey) {
+        if (helix) {
             animate();
         }
     }
@@ -112,18 +123,49 @@ document.addEventListener("DOMContentLoaded", () => {
     });*/
     var monkeyMaterial = new THREE.MeshStandardMaterial({color: 0x5590d5});
     const loader = new GLTFLoader();
-    loader.load( "./monkey.glb", function (object) {
+    loader.load( "./spiral_pillar.glb", function (object) {
         /*object.scene.traverse( function( child ) {
             if ( child instanceof THREE.Mesh ) {
                 child.material = monkeyMaterial;
             }
-        });*/
+        });*/ 
+
+        helix = object.scene.children[1]; // store the object's mesh
         scene.add(object.scene);
-        monkey = object.scene.children[2]; // store the monkey's mesh
-        object.scene.children[2].material = new THREE.MeshStandardMaterial({color: 0xffddff});
-        object.scene.children[2].material.onBeforeCompile = function ( shader ) {
+
+        // Edge split modifier
+        /*edgeSplitModifier = new EdgeSplitModifier();
+        console.log(edgeSplitModifier);
+        baseGeometry = BufferGeometryUtils.mergeVertices(helix.geometry);
+        var cutOffAngle = 30;
+        edgeSplitModifier.modify(
+            baseGeometry,
+            cutOffAngle * Math.PI / 180
+        );*/
+        //helix.geometry = edgeSplitModifier;
+
+        //console.log("baseGeometry", baseGeometry);
+        //console.log("edgeSplitModifier", edgeSplitModifier);
+        
+
+        //mesh = new THREE.Mesh( baseGeometry, new THREE.MeshStandardMaterial() );
+        //mesh.geometry = helix.geometry;
+        //scene.add(mesh);
+        
+        object.scene.children[0].material = new THREE.MeshStandardMaterial({
+            color: 0xffddff,
+            roughness: 0.0
+        });
+        object.scene.children[1].material = new THREE.MeshStandardMaterial({
+            color: 0xffddff,
+            roughness: 0.0,
+            side: THREE.DoubleSide
+        });
+        object.scene.children[1].material.onBeforeCompile = function ( shader ) {
             shader.uniforms.time = { value: currentTime };
             shader.uniforms.resolution = { value: new THREE.Vector2(displayWidth, displayHeight) };
+           
+            //shader.uniform.emissive = { value: new THREE.Color(0.9, 0.1, 0.2) };
             
             /*shader.vertexShader = 'varying vec4 vWorldPosition;\n' + shader.vertexShader;
             shader.vertexShader = shader.vertexShader.replace(
@@ -150,7 +192,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }, undefined, function (error) {
         console.error(error);
     });
-    //console.log(monkey);
+    //console.log(helix);
 
     /*const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
